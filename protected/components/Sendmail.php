c6799a8c64ab7d538186c3eb58b9881148731c71
ianikanov <ivan.anikanov@gmail.com>
2015-07-06 23:02:59 +0300

    update mail to new host

--- protected/components/Sendmail.php
+++ protected/components/Sendmail.php
@@ -2,7 +2,7 @@
 class Sendmail extends CController
 {
 	public static $mailPrefix = "WKF.Task ";
-	public static $baseUrl = "http://wkf.bit.ru/wkf.task/";
+	public static $baseUrl = "http://wkfteam.ru/wkf.task/";
 	
 	private static function normalizeUrl($arr)
 	{
@@ -26,13 +26,15 @@ class Sendmail extends CController
 				'params' => array(':pid'=>$project->id),
 				'order' => 'owner_user_id'
 			));
-			$message = $instance->renderInternal("views/SL_projectStatus.php", array('ticketsClosed'=>$ticketsClosed,'ticketsOverdue'=>$ticketsOverdue,'projectName'=>$project->name), true);
-			$result = Sendmail::mail(User::model()->findAll(array(
-				'join'=>'LEFT OUTER JOIN usergroup AS ug ON t.usergroup_id = ug.id
-							LEFT OUTER JOIN user_has_project AS up ON t.id = up.user_id',
-				'condition'=>'ug.level >= 10 AND t.notification_enabled = 1 AND up.project_id = :pid AND up.get_notifications = 1',
-				'params'=>array(':pid'=>$project->id)
-			)), "Статус ".date("d.m.Y")." по проекту ".$project->name, $message);
+			if (count($ticketsClosed) > 0 || count($ticketsOverdue) > 0) {
+				$message = $instance->renderInternal("views/SL_projectStatus.php", array('ticketsClosed'=>$ticketsClosed,'ticketsOverdue'=>$ticketsOverdue,'projectName'=>$project->name), true);
+				$result = Sendmail::mail(User::model()->findAll(array(
+					'join'=>'LEFT OUTER JOIN usergroup AS ug ON t.usergroup_id = ug.id
+								LEFT OUTER JOIN user_has_project AS up ON t.id = up.user_id',
+					'condition'=>'ug.level >= 10 AND t.notification_enabled = 1 AND up.project_id = :pid AND up.get_notifications = 1',
+					'params'=>array(':pid'=>$project->id)
+				)), "Статус ".date("d.m.Y")." по проекту ".$project->name, $message);
+			}
 		}
 	}
 	
