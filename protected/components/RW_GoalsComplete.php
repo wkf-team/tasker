16421f5f4d9f7634387bfe1baed2fc4406792d8e
ianikanov <ivan.anikanov@gmail.com>
2015-09-25 14:36:14 +0300

    remove tasks from not active projects from main lists

--- protected/components/RW_GoalsComplete.php
+++ protected/components/RW_GoalsComplete.php
@@ -5,9 +5,10 @@ class RW_GoalsComplete extends CWidget {
  
     public function run() {
 		$this->goals = VGoalsComplete::model()->findAll(array(
-			'condition'=>'p.user_id = :uid',
+			'condition'=>'p.user_id = :uid AND pr.is_active = 1',
 			'join'=>'INNER JOIN ticket AS tc ON t.id = tc.id
-					INNER JOIN user_has_project AS p ON p.project_id = tc.project_id',
+					INNER JOIN user_has_project AS p ON p.project_id = tc.project_id
+					INNER JOIN project AS pr ON p.project_id = pr.id',
 			'params'=>array(':uid'=>Yii::app()->user->id),
 		));
         $this->render('RWUI_goalsComplete');
--- protected/components/TW_MyTasks.php
+++ protected/components/TW_MyTasks.php
@@ -11,7 +11,10 @@ class TW_MyTasks extends CWidget {
 		$dataProvider=new CActiveDataProvider('Ticket', array(
 			'criteria'=>array(
 				// открытые цели, незакрытые задач
-				'condition'=>'(status_id < 6 OR status_id = 6 AND end_date > SUBDATE(NOW(), 14)) AND ticket_type_id > 1 AND (owner_user_id = '.$id.' OR responsible_user_id = '.$id.')',
+				'condition'=>'(status_id < 6 OR status_id = 6 AND end_date > SUBDATE(NOW(), 14)) AND ticket_type_id > 1 AND
+					p.is_active = 1 AND
+					(owner_user_id = '.$id.' OR responsible_user_id = '.$id.')',
+				'join'=>'INNER JOIN project AS p ON t.project_id = p.id',
 				'order'=>Ticket::$orderString,
 			),'pagination'=>array(
 				'pageSize'=>20,
