de8763584cdf3f8779a36421c58ab7467496d614
ianikanov <ivan.anikanov@gmail.com>
2019-09-15 16:43:47 +0100

    Вывод целей только по активным

--- protected/data/alter.sql
+++ protected/data/alter.sql
@@ -1,23 +1,20 @@
 CREATE 
-     OR REPLACE 
-VIEW `v_goals_complete` AS
-     SELECT 
+ OR REPLACE VIEW `v_goals_complete` AS
+    SELECT 
         `goals`.`id` AS `id`,
         `goals`.`subject` AS `subject`,
         `goals`.`project_id` AS `project_id`,
-        (COUNT(DISTINCT `tasks`.`id`)
-		) AS `total`,
-        (SUM(IF((`tasks`.`status_id` IN (4,5)), 1, 0))
-        )AS `progress`,
-        (SUM(IF((`tasks`.`status_id` >= 6), 1, 0))
-        )AS `closed`,
+        `goals`.`status_id`,
+        COUNT(DISTINCT `tasks`.`id`) AS `total`,
+        SUM(IF((`tasks`.`status_id` IN (4 , 5)),
+            1,
+            0)) AS `progress`,
+        SUM(IF((`tasks`.`status_id` >= 6), 1, 0)) AS `closed`,
         `goals`.`estimate_start_date` AS `start_date`,
         `goals`.`due_date` AS `due_date`
     FROM
         (`ticket` `goals`
-        LEFT JOIN `ticket` `tasks` ON ((`tasks`.`parent_ticket_id` = `goals`.`id`))
-        
-        )
+        LEFT JOIN `ticket` `tasks` ON ((`tasks`.`parent_ticket_id` = `goals`.`id`)))
     WHERE
         (((`goals`.`status_id` < 6)
             OR ((`goals`.`status_id` = 6)
